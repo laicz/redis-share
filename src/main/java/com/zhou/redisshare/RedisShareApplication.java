dbc133030ee0af20b55585cc827bc65f294eecca
zhoumb <zhoumb@vibeac.com>
2018-12-28 15:51:17 +0800

    创建Redis学习项目

---
+++ src/main/java/com/zhou/redisshare/RedisShareApplication.java
@@ -0,0 +1,14 @@
+package com.zhou.redisshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisShareApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/zhou/redisshare/RedisShareApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zhou.redisshare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisShareApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
